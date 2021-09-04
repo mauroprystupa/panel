c0911ed786b86b8cb873862116c30f2eb851ee23
Mauro Sebastian Prystupa <81568612+mauroprystupa@users.noreply.github.com>
2021-09-04 13:12:56 -0300

    Update conexiondb.php
    <?php
    
    function conexion_db(){
    
    //Base de datos local
    /* $basededatos = "dashhtml";
    $usuario = "root" ;
    $contrasena = "";
    $conexion = "localhost"; */
    
    
    //Hostinger Mysql
    $basededatos = "u463129590_prueba_gb";
    $usuario = "u463129590_gaston" ;
    $contrasena = "Ns2b7bfqbf!";
    $conexion = "sql234.main-hosting.eu";
    
    
    $cadena_conexion = new mysqli($conexion,$usuario,$contrasena,$basededatos);
    
    return $cadena_conexion;
    
    }
    
    
    
    ?>

--- conexiondb.php
+++ conexiondb.php
@@ -1,18 +1,1 @@
-<?php
 
-function conexion_db(){
-
-$basededatos = "dashhtml";
-$usuario = "root" ;
-$contrasena = "";
-$conexion = "localhost";
-
-$cadena_conexion = new mysqli($conexion,$usuario,$contrasena,$basededatos);
-
-return $cadena_conexion;
-
-}
-
-
-
-?>
